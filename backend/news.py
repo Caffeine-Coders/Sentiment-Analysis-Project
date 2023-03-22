4804854bcbbbe226f72ed7a30d869926f3a94799
DheerajNani7 <60538004+DheerajNani7@users.noreply.github.com>
2023-03-22 16:50:45 -0400

    added news

---
+++ backend/news.py
@@ -0,0 +1,14 @@
+from newsapi import NewsApiClient
+newsapi = NewsApiClient(api_key='4f9f84129c964ac9a6c6687006a2cc22')
+all_articles = newsapi.get_everything(
+    q='dhoni',
+    language='en',
+)
+ct=0
+for article in all_articles['articles']:
+    ct+=1
+    print('url: ',article['url'])
+    print('Source : ',article['source']['name'])
+    print('Title : ',article['title'])
+    print('Description : ',article['description'], '\n\n')
+print(ct)
